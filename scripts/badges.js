db89282eb09ae8496ab24decae7f6d43dddadc00
Malte Heidemann <hei@ordix.de>
2020-10-22 10:42:51 +0200

    ✨ extended script to create badge

--- scripts/badges.js
+++ scripts/badges.js
@@ -1,2 +1,17 @@
+const { makeBadge } = require('badge-maker');
+const fs = require('fs');
+
 const version = require('../package.json').version;
-console.log(version);
+const svgVersion = makeBadge({
+  label: 'Version',
+  message: version,
+  color: 'blue',
+});
+
+fs.mkdir('badges', err => {
+  if (err && err.errno !== -17) console.error(err);
+});
+
+fs.writeFile('badges/version.svg', svgVersion, err => {
+  if (err) console.error(err);
+});
