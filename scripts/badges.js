a2ed7c4ad529a512e3cb0ce3e818cc17c44221d8
Malte Heidemann <hei@ordix.de>
2020-10-23 15:57:34 +0200

    ✨ extended script

--- scripts/badges.js
+++ scripts/badges.js
@@ -1,11 +1,25 @@
 const { makeBadge } = require('badge-maker');
 const fs = require('fs');
+const npmCheck = require('npm-check');
 
 const version = require('../package.json').version;
 const svgVersion = makeBadge({
   label: 'Version',
   message: version,
   color: 'blue',
+});
+
+npmCheck().then(state => {
+  const numOutdated = state.get('packages').filter(pkg => !!pkg.bump).length;
+  const svgOutdated = makeBadge({
+    label: 'Outdated',
+    message: String(numOutdated),
+    color: (numOutdated === 0 ? 'green' : 'red'),
+  });
+
+  fs.writeFile('badges/outdated.svg', svgOutdated, err => {
+    if (err) console.error(err);
+  });
 });
 
 fs.mkdir('badges', err => {
